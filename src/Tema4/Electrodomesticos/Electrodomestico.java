846d84bc9fc454cb0fc7de19798c8aab2737eb0a
GJC <carlagk04@hotmail.com>
2026-02-09 18:47:02 +0100

    Electrodomestico

---
+++ src/Tema4/Electrodomesticos/Electrodomestico.java
@@ -0,0 +1,9 @@
+package Tema4.Electrodomesticos;
+
+public class Electrodomestico {
+    protected double precioBase;
+    protected String color;
+    protected char consumoEnergetico;
+    protected double peso;
+
+}
