1e53fe8bef6d3635692ffb49f12905200d272342
GJC <carlagk04@hotmail.com>
2026-03-16 18:20:05 +0100

    Tema 5 ejer ElectroAvilaMain

---
+++ src/Tema5/electroavila/ElectroAvilaMain.java
@@ -0,0 +1,53 @@
+package Tema5.electroavila;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ElectroAvilaMain {
+    public static void main(String[] args) {
+
+        Map<String, Map<String, Integer>> ventasPorZona = new HashMap<>();
+        ventasPorZona.putIfAbsent("Avila", new HashMap<>());
+        ventasPorZona.putIfAbsent("Burgos", new HashMap<>());
+        ventasPorZona.putIfAbsent("Segovia", new HashMap<>());
+        ventasPorZona.get("Avila").put("Luis", 4500);
+        ventasPorZona.get("Avila").put("Fernando", 3200);
+        ventasPorZona.get("Avila").put("Marta", 7000);
+        ventasPorZona.get("Segovia").put("Rigoberto", 6200);
+        ventasPorZona.get("Segovia").put("Luciano", 5200);
+        ventasPorZona.get("Segovia").put("Marta", 1200);
+        ventasPorZona.get("Burgos").put("Fernando", 4500);
+        ventasPorZona.get("Burgos").put("Rigoberto", 7200);
+
+        //añadir a fernando 600 en ventas em burgos
+       /* ventasPorZona.get("Burgus").put("Fernando", 600);
+        System.out.println(ventasPorZona);
+        Integer value = ventasPorZona.get("Burgos").getOrDefault("Fernando", 0);
+        ventasPorZona.get("Burgos").put("Fernando", value + 600);
+        System.out.println(ventasPorZona);
+        */
+        // Transformar en ventas por vendedor
+        // Luis > Avila > 4500
+        // Fernando > Avila > 3200
+        // Fernando > Burgos > 4500
+        Map<String, Map<String, Integer>> ventasPorVendedor = new HashMap<>();
+
+        Set<String> zonas = ventasPorZona.keySet();
+
+        for (String zona : zonas) {
+            Set<String> vendedores = ventasPorZona.get(zona).keySet();
+            for (String vendedor : vendedores) {
+                Integer valor = ventasPorZona.get(zona).get(vendedor);
+                System.out.println(zona + " :: " + vendedor + " :: " + valor);
+
+                if (!ventasPorVendedor.containsKey(vendedor))
+                    ventasPorVendedor.put(vendedor, new HashMap<>());
+                ventasPorVendedor.get(vendedor).put(zona, valor);
+
+            }
+
+        }
+
+    }
+}
