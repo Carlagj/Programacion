560a41dd2b0b424d26b5591edf6517f309da50d3
GJC <carlagk04@hotmail.com>
2026-03-16 16:41:50 +0100

    Tema 5 ejer5

---
+++ src/Tema5/Ejercicios5/Producto.java
@@ -0,0 +1,56 @@
+package Tema5.Ejercicios5;
+
+import java.util.Objects;
+
+public class Producto {
+    private String id;
+    private String modelo;
+    private double precioBase;
+    private int stock;
+
+    public Producto(String id, String modelo, double precioBase, int stock) {
+        this.id = id;
+        this.modelo = modelo;
+        this.precioBase = precioBase;
+        this.stock = stock;
+    }
+
+    // Getters y Setters básicos
+    public String getId() {
+        return id;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precio) {
+        this.precioBase = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %-10s | Modelo: %-15s | Precio: %8.2f€ | Stock: %d",
+                id, modelo, precioBase, stock);
+    }
+
+    // TODO: TAREA DEL ALUMNO
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Producto producto)) return false;
+        return Objects.equals(id, producto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    // Implementar equals y hashCode basándose ÚNICAMENTE en el 'id'.
+    // Recordad que sin esto, el Set no detectará duplicados.
+}
