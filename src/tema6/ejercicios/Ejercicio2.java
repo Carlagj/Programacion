476e3b864b1c22467122a393763a5b8228ecc2d4
GJC <carlagk04@hotmail.com>
2026-04-15 20:00:44 +0200

    exam

--- src/tema6/ejercicios/Ejercicio2.java
+++ src/tema6/ejercicios/Ejercicio2.java
@@ -1,5 +1,10 @@
 package tema6.ejercicios;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ejercicio2 {
     /*
     Ejercicio 2: El Diario de Clase (Escritura de Texto Plano)
@@ -12,4 +17,33 @@ Acción: 1. Cada frase introducida se debe guardar en un archivo llamado diario.
 3. Asegúrate de que si ejecutas el programa dos veces,
 las frases nuevas se añadan debajo de las antiguas sin borrar las anteriores (modo append).
      */
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        try {
+
+            FileWriter fw = new FileWriter("diario.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            String frase = "";
+
+            while (!frase.equalsIgnoreCase("FIN")) {
+                System.out.println("Escribe una frase (o 'FIN' para terminar):");
+                frase = sc.nextLine();
+                if (!frase.equalsIgnoreCase("FIN")) {
+                    bw.write(frase);
+                    bw.newLine();
+                }
+            }
+
+            bw.close();
+            System.out.println("Guardado con éxito.");
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo.");
+        }
+
+        sc.close();
+    }
 }
