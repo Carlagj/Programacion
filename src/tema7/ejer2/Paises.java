49506eb036209dacca78f4e89592e1c6cf1ed90a
GJC <carlagk04@hotmail.com>
2026-04-22 16:54:24 +0200

    exam

---
+++ src/tema7/ejer2/Paises.java
@@ -0,0 +1,12 @@
+package tema7.ejer2;
+
+import java.sql.Connection;
+
+public class Paises {
+    private static final String URL = "jdbc:mysql://localhost:3306/geografia_db";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = ""; // Que cada alumno ponga la suya
+    private Connection conexion;
+
+
+}
